d376964cb5e14fed0092ab74b8a11f3048d2d012
Ayush Goyal <agoyal2001@gmail.com>
2017-04-01 13:02:33 -0400

    Simplify index.js

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-var http = require('http');
-var path = require('path');
 var express = require('express');
-var morgan = require('morgan');
-var mongoose = require('mongoose');
-var urlencoded = require('body-parser').urlencoded;
-var config = require('./config');
-var voice = require('./routes/voice');
-var message = require('./routes/message');
-var results = require('./routes/results');
-
-// initialize MongoDB connection
-mongoose.connect(config.mongoUrl);
-
-// Create Express web app with some useful middleware
+var bodyParser = require('body-parser');
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(urlencoded({ extended: true }));
-app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-// Twilio Webhook routes
-app.post('/voice', voice.interview);
-app.post('/voice/:responseId/transcribe/:questionIndex', voice.transcription);
-app.post('/message', message);
+app.post("/message", function (request, response) {
+  console.log(request.body);
+  response.send("<Response><Message>Hello</Message></Response>")
+});
 
-// Ajax route to aggregate response data for the UI
-app.get('/results', results);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
 
-// Create HTTP server and mount Express app
-var server = http.createServer(app);
-server.listen(config.port, function() {
-    console.log('Express server started on *:'+config.port);
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
