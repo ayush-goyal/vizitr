7d85dbc6fc4d40858e0a298cc9e5d7446811db4b
Ayush Goyal <agoyal2001@gmail.com>
2017-04-01 13:42:35 -0400

    Add other questions

--- index.js
+++ index.js
@@ -10,9 +10,26 @@ app.post("/message", function (request, response) {
   console.log(request.body);
   if (request.body.From in numbers) {
     console.log("In numbers");
+    if (numbers[request.body.From]["first"] == '') {
+      numbers[request.body.From]["first"] = request.body.Body;
+      response.send("<Response><Message>What is your last name?</Message></Response>")
+    } else if (numbers[request.body.From]["last"] == '') {
+      numbers[request.body.From]["last"] = request.body.Body;
+      response.send("<Response><Message>What is the reason for your visit?</Message></Response>")
+    } else if (numbers[request.body.From]["reason"] == '') {
+      numbers[request.body.From]["reason"] = request.body.Body;
+      response.send("<Response><Message>Great! Please proceed to security with this QR Code</Message></Response>")
+    }
+    console.log(numbers[request.body.From]);
   } else {
-    console.log("not in numbers");
-    numbers[request.body.From] = [];
+    console.log("Not in numbers");
+    numbers[request.body.From] = {
+      "first": '',
+      "last": '',
+      "reason": ''
+    };
+    console.log(numbers[request.body.From]);
+    response.send("<Response><Message>What is your first name?</Message></Response>")
   }
   response.send("<Response><Message>Hello</Message></Response>")
 });
